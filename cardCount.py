ae10e60ecbee2cf32034ee4db35e650e082cd275
Anay Bhakat <38054625+abhakat@users.noreply.github.com>
2024-08-12 15:42:03 -0700

    basic deck setup and start of calculator

--- cardCount.py
+++
@@ -1,8 +0,0 @@
-from playingcards import Deck, Card
-
-player_deck = Deck()
-player_card = player_deck.draw_card()
-player_hand = player_deck.draw_n(5)
-print(player_hand)
-
-### so what do we need to do??
