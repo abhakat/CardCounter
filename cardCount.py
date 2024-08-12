f48489566210eff1059db632be4738cd53093099
Anay Bhakat <38054625+abhakat@users.noreply.github.com>
2024-08-12 01:50:05 -0700

    Initial commit

---
+++ cardCount.py
@@ -0,0 +1,8 @@
+from playingcards import Deck, Card
+
+player_deck = Deck()
+player_card = player_deck.draw_card()
+player_hand = player_deck.draw_n(5)
+print(player_hand)
+
+### so what do we need to do??
